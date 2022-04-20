67082cd39ab49f461201860486be1b9c30ef728d
Oshin Chiong <oshinchiong@Oshins-MacBook-Air.local>
2022-04-20 18:00:35 -0400

    itiration2

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ require('./db');
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require('express');
+const mongoose = require("mongoose");
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
 
 const app = express();
+require("./config/session.config")(app);
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
@@ -27,9 +29,18 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+var usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
+
+mongoose
+  .connect("mongodb://localhost/lab-express-basic-auth")
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 module.exports = app;
 
---
+++ routes/users.js
@@ -0,0 +1,87 @@
+var express = require("express");
+var router = express.Router();
+const User = require("../models/User.model");
+const bcrypt = require("bcryptjs");
+const res = require("express/lib/response");
+const saltRounds = 10;
+
+router.get("/signup", function (req, res, next) {
+    res.render("create-user");
+  });
+router.get("/login", function (req, res, next){
+res.render("login");
+});
+
+router.post("/signup", function (req, res, next) {
+    console.log("hit", req.body);
+    if (!req.body.username || !req.body.password) {
+        res.render("create-user", { message: "Username and password requiered"})
+    }
+
+User.findOne({ username: req.body.username })
+.then((foundUser) => {
+    console.log("Found user", foundUser);
+    if (foundUser) {
+        res.render("create-user", { message: "username is taken"})
+    } else {
+    const salt = bcrypt.genSaltSync(saltRounds);
+    console.log("SALT", salt);
+
+    const hashedPass = bcrypt.hashSync(req.body.password, salt);
+    console.log("hashedPass", hashedPass);
+
+    User.create({
+     username: req.body.username,
+     password: hashedPass,
+     })
+      .then((createdUser) => {
+        res.redirect("/users/signup");
+     })
+      .catch((err) => {
+    console.log("error creating user".err.message)
+        });
+        }
+    })
+});
+
+router.post("/login", (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        req.render("login", { message: "Username and password requiered"})
+    }
+
+    User.findOne({ username: req.body.username })
+    .then((foundUser) => {
+    if (!foundUser) {
+    res.render("login", {message: "Username not found"});
+} else {
+    let correctPassword = bcrypt.compareSync(
+    req.body.password,
+    foundUser.password
+ );
+ if (correctPassword) {
+     req.session.user = foundUser;
+     res.json(req.session);
+ } else {
+     res.render("login", { message: "Incorrect password"})
+ }
+}
+ })
+ .catch((error) => {
+     console.log("Login failed", error.message);
+ });
+});
+
+router.get("/test-login", (req, res) => {
+    if (req.session.user) {
+        res.json(req.session);
+    } else {
+        res.status(402).json("You are not logged in");
+    }
+});
+
+router.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.json("You have logout");
+});
+
+module.exports = router;
